1064ec1b5444819c71388452f0cb1634dd375e68
ValerioGc <valerio.gc1@gmail.com>
2022-11-15 14:46:41 +0100

    Avanzamento

--- src/org/generation/italy/CalcolaBiglietto.java
+++ src/org/generation/italy/CalcolaBiglietto.java
@@ -1,5 +1,23 @@
 package org.generation.italy;
 
-public class CalcolaBiglietto {
+import java.util.Scanner;
 
+public class CalcolaBiglietto {
+	
+	public static void main(String[] args) {
+		
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Inserisci la distanza da percorrere");
+		String userDistance = scn.nextLine();
+		System.out.println("Inserisci la tua età");
+		int userAge = scn.nextInt();
+		scn.close();		
+		
+		float kmPrice = 0.21f;
+		
+		System.out.println("L'utente ha " + userAge + "anni e deve percorrere " + userDistance );
+		
+		
+		
+	}
 }
