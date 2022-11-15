01d172a7b58cb2a5467440d0ad46e7e685ce482a
ValerioGc <valerio.gc1@gmail.com>
2022-11-15 15:03:09 +0100

    fine esercizio

--- src/org/generation/italy/CalcolaBiglietto.java
+++ src/org/generation/italy/CalcolaBiglietto.java
@@ -7,17 +7,27 @@ public class CalcolaBiglietto {
 	public static void main(String[] args) {
 		
 		Scanner scn = new Scanner(System.in);
-		System.out.println("Inserisci la distanza da percorrere");
-		String userDistance = scn.nextLine();
+		System.out.println("Inserisci la distanza da percorrere in km"); 
+		int userDistance = scn.nextInt();
 		System.out.println("Inserisci la tua età");
 		int userAge = scn.nextInt();
 		scn.close();		
 		
 		float kmPrice = 0.21f;
+		int underDiscount = 20;
+		int overDiscount = 40;
+		String eur = "\u20ac";
+		System.out.println("L'utente ha " + userAge + " anni e deve percorrere " + userDistance );
+		float finalPrice =  userDistance * kmPrice;
 		
-		System.out.println("L'utente ha " + userAge + "anni e deve percorrere " + userDistance );
-		
-		
-		
+		if (userAge > 18) {
+			float underPrice = finalPrice * underDiscount / 100;
+			System.out.println(underPrice);
+		} else if (userAge >= 65) {
+			float overPrice = finalPrice * overDiscount / 100;
+			System.out.println(finalPrice);
+		} else {
+			System.out.println("Il costo del biglietto è di: " + finalPrice + "\u20ac" + eur);
+		}
 	}
 }
